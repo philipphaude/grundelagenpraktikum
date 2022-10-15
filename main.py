74e7ec7058ecd4e409c43afad6a7b76ec7485d65
Steffen Maßmann <steffen.massmann@tu-dortmund.de>
2022-10-15 17:04:15 +0200

    Ich liebe Kekse! Meter in cm geändert :^)

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import csv
-
 import matplotlib.pyplot as plt
 
 with open('data.csv', newline='') as csvfile:
     data = list(csv.reader(csvfile))
 
-
 # filter only first two columns
 data = [row[:2] for row in data]
 
-#remove first character from each element
+# remove first character from each element
 data = [[row[0][1:], row[1][1:]] for row in data]
 
-#convert to float
+# convert to float
 data = [[float(row[0]), float(row[1])] for row in data]
 
 # plot data as scatter plot with cross markers
 plt.scatter([row[0] for row in data], [row[1] for row in data], marker='x')
-# add labels
-plt.xlabel(r'd in $[m]$')
+plt.xlabel(r'd in $[cm]$')
 plt.ylabel(r'N in $[\frac{1}{60s}]$')
-# scale y axis logarithmic
 plt.savefig('plot_1.pdf')
+plt.show()
 plt.yscale('log')
-# save plot as pdf
 plt.savefig('plot_2.pdf')
 plt.show()
-
-
-print(data)
